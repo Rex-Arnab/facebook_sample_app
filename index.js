8b8c39bf95cb49884f8abd9157febc1b9c51fe09
Arnab Biswas <khanp4397@gmail.com>
2024-02-05 16:17:37 +0530

    removed socket

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+// const socketIo = require('socket.io');
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -8,38 +8,38 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+// const io = socketIo(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Handle connection
-io.on('connection', (socket) => {
-    console.log('A user connected');
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
 
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+//     // Handle disconnection
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
 
-    // Handle chat message event
-    socket.on('message', (msg) => {
-        if (msg === 'hi bhai') {
-            io.emit('message', 'Hello!');
-        } else if (msg === 'bye') {
-            io.emit('message', 'Goodbye bhai!');
-        }
-    });
+//     // Handle chat message event
+//     socket.on('message', (msg) => {
+//         if (msg === 'hi bhai') {
+//             io.emit('message', 'Hello!');
+//         } else if (msg === 'bye') {
+//             io.emit('message', 'Goodbye bhai!');
+//         }
+//     });
 
-    // make echo event
-    socket.on('echo', (data) => {
-        console.log('Data received:', data);
-        // Emit data back to the client
-        io.emit('echo', data);
+//     // make echo event
+//     socket.on('echo', (data) => {
+//         console.log('Data received:', data);
+//         // Emit data back to the client
+//         io.emit('echo', data);
 
-    });
-});
+//     });
+// });
 
 // Import Routes
 const facebookRoutes = require('./Routes/facebook');
