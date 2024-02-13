217af719be68e3a87505b2652e8bd4fda4fb3d5b
Arnab Biswas <arnab@zithara.in>
2024-02-13 14:25:08 +0530

    added working prototype

--- index.js
+++ index.js
@@ -50,11 +50,20 @@ app.use('/insta', instagramRoutes);
 app.use('/webhook', webhookRoutes);
 
 app.get('/', (_req, res) => {
+    const APP_ID = process.env.ACCESS_TOKEN;
+    const redirect_url = process.env.HOST_URL + '/fbcallback';
+    const CONFIG_ID = process.env.CONFIG_ID;
+
     res.json({
-        message: 'Hello, User!'
+        message: 'Please Login thought this url to initate facebook login for business account',
+        url: `https://www.facebook.com/v19.0/dialog/oauth?client_id=${APP_ID}&redirect_uri=${redirect_url}&state=testing&response_type=code&config_id=${CONFIG_ID}`
     });
 });
 
-server.listen(PORT, () => {
+app.get('/fbcallback', (req, res) => {
+    return res.json(req.query);
+});
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
